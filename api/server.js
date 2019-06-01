ee8ba5a3b9adaf6a980705e0ef4ad8bb18d92f44
Husot <email@example.com>
2019-06-01 13:40:27 -0400

    init knex & 1st project table added with knex

---
+++ api/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const helmet = require('helmet');
+
+const projectsRouter = require('../projects/projects-router');
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+
+server.use('/api/projects', projectsRouter);
+
+// sanity check route
+server.get('/', (req, res) => {
+  res.status(200).json({ hello: 'World!' });
+});
+
+module.exports = server;
